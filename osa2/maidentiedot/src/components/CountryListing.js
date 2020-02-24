a088d5d399a18e9a4096a3177a4f5c72d1c9a967
Joel Lavikainen <joel.lavikainen@gmail.com>
2020-02-24 19:52:36 +0200

    Add show button to country listing

--- osa2/maidentiedot/src/components/CountryListing.js
+++ osa2/maidentiedot/src/components/CountryListing.js
@@ -1,13 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
+import SingleCountryListing from "./SingleCountryListing"
 
 const CountryListing = ({ countries }) => {
-  return (
-    <ul>
-      {countries.map(country => (
-        <li key={country.name}>{country.name}</li>
-      ))}
-    </ul>
-  )
+  const [single, setSingle] = useState("")
+  if (single) {
+    return <SingleCountryListing country={single} />
+  } else {
+    return (
+      <ul>
+        {countries.map(country => (
+          <li key={country.name}>
+            {country.name}{" "}
+            <button onClick={e => setSingle(country)}>show</button>
+          </li>
+        ))}
+      </ul>
+    )
+  }
 }
 
 export default CountryListing
