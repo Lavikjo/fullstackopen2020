67e4efaeea9d8e111884903e89ae410cf68599c4
Joel Lavikainen <joel.lavikainen@gmail.com>
2020-04-07 20:31:39 +0300

    Add logging middleware

--- osa3/phonebook_backend/index.js
+++ osa3/phonebook_backend/index.js
@@ -1,8 +1,10 @@
 const express = require("express")
+const morgan = require('morgan')
 const cors = require("cors")
 const app = express()
 const bodyParser = require("body-parser")
 
+app.use(morgan('tiny'))
 app.use(bodyParser.json())
 
 app.use(cors())
