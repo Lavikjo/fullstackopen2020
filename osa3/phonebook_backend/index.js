c9838624ef22d769f8727404bfc5da33af87c9f4
Joel Lavikainen <joel.lavikainen@gmail.com>
2020-04-09 14:31:38 +0300

    Add error handling middleware and PUT

--- osa3/phonebook_backend/index.js
+++ osa3/phonebook_backend/index.js
@@ -11,27 +11,15 @@ morgan.token("body", function getBody(req) {
   return JSON.stringify(req.body)
 })
 
+
 app.use(express.static("build"))
 app.use(bodyParser.json())
 app.use(morgan(":method :url :status :response-time ms :body"))
 app.use(cors())
 
-const generateId = () => {
-  /*const maxId = persons.length > 0
-      ? Math.max(...persons.map(n => n.id))
-      : 0
-    return maxId + 1
-    */
-  /* Why Math.random, better solution would be
-    import { v4 as uuidv4 } from 'uuid';
-    const uuid = uuidv4();
-    return uuid
-   */
-  const id = Math.random() * Number.MAX_SAFE_INTEGER
-  return id
-}
 
-app.get("/info", (request, response) => {
+
+app.get("/info", (request, response, next) => {
   Person.find({}).then((persons) => {
     const time = new Date()
     response.send(`<div>Phonebook has info for ${persons.length} persons</div>
@@ -40,32 +28,30 @@ app.get("/info", (request, response) => {
   
 })
 
-app.get("/api/persons", (request, response) => {
+app.get("/api/persons", (request, response, next) => {
   Person.find({}).then((persons) => {
     response.json(persons.map((person) => person.toJSON()))
   })
 })
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => {
       response.json(person.toJSON())
     })
-    .catch(function (error) {
-      return response.status(404).end()
-    })
+    .catch((error) => next(error)
+    )
 })
 
-app.delete("/api/persons/:id", (request, response) => {
-  Person.findByIdAndRemove(request.params.id, request.body, function(err, data) {
-    if(err)
-      return response.status(404).end()
-    else
-      return response.status(204).end()
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
   })
+  .catch(error => next(error))
 })
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body
   if (!body.name && !body.number) {
     return response.status(400).json({
@@ -90,6 +76,39 @@ app.post("/api/persons", (request, response) => {
     response.json(savedPerson.toJSON())
   })
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson.toJSON())
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
