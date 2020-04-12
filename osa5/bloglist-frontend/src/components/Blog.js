3f7672fd575d84248ce3a6fadf516bd5c5c6cc49
Joel Lavikainen <joel.lavikainen@gmail.com>
2020-04-12 15:48:01 +0300

    Add toggleable additional info

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,33 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from "react"
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const buttonText = visible ? "hide" : "view"
+  return (
+    <>
+      <div style={blogStyle}>
+        {blog.title} {blog.author}{" "}
+        <button onClick={toggleVisibility}>{buttonText}</button>
+        <div>{visible ? blog.url : null}</div>
+        <div>
+          {visible ? blog.likes : null}
+          {visible ? <button>Like</button> : null}
+        </div>
+        <div>{visible ? blog.user.name : null}</div>
+      </div>
+    </>
+  )
+}
 
 export default Blog
