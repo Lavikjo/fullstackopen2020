b944ff1986b42bc1af6c00549e72f0a95d6db21e
Joel Lavikainen <joel.lavikainen@gmail.com>
2020-04-22 23:37:36 +0300

    Refactor form to use custom hook

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -3,6 +3,8 @@ import {
   Switch, Route, Link, useRouteMatch, useHistory
 } from "react-router-dom"
 
+import { useField } from "./hooks/index"
+
 const Menu = () => {
   const padding = {
     paddingRight: 5
@@ -67,17 +69,17 @@ const Notification = ({notification}) => (
 
 const CreateNew = (props) => {
   const history = useHistory()
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField("text")
+  const author = useField("text")
+  const info = useField("text")
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     history.push("/")
@@ -89,15 +91,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
