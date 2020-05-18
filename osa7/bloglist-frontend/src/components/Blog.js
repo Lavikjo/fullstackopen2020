dd7bf781a121c6cc7b1e15be57c0d702346bd50d
Joel Lavikainen <9072078+Lavikjo@users.noreply.github.com>
2020-05-18 23:40:16 +0300

    Make bloglist link to individual view

--- osa7/bloglist-frontend/src/components/Blog.js
+++ osa7/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,4 @@
 import React from "react"
-import { like, removeBlog } from "../reducers/blogReducer"
-import { toggleBlogVisibility } from "../reducers/visibilityReducer"
-import { useDispatch, useSelector } from "react-redux"
-
 
 const Blog = ({ blog }) => {
   const blogStyle = {
@@ -12,44 +8,10 @@ const Blog = ({ blog }) => {
     borderWidth: 1,
     marginBottom: 5,
   }
-  const visible = useSelector(state => state.visibility.blogs[blog.id])
-  const dispatch = useDispatch()
-
-  const buttonText = visible ? "hide" : "view"
   return (
     <>
       <div style={blogStyle} className="blog">
-        {blog.title} {blog.author}{" "}
-        <button onClick={() => dispatch(toggleBlogVisibility(blog.id))}>{buttonText}</button>
-        <div>{visible ? blog.url : null}</div>
-        <div>
-          {visible ? blog.likes : null}
-          {visible ? (
-            <button className="likeButton"
-              onClick={() => dispatch(like(blog))
-              }>
-              Like
-            </button>
-          ) : null}
-        </div>
-        <div>{visible ? blog.user.name : null}</div>
-        <div>
-          {visible &&
-          blog.user.username ===
-            JSON.parse(window.localStorage.getItem("loggedBlogappUser"))
-              .username ? (
-              <button
-                onClick={async () => {
-                  if (
-                    window.confirm(`Remove blog ${blog.title} by ${blog.author}?`)
-                  ) {
-                    dispatch(removeBlog(blog.id))
-                  }
-                }}>
-              Delete
-              </button>
-            ) : null}
-        </div>
+        <a href={`/blogs/${blog.id}`}>{blog.title} {blog.author}</a>
       </div>
     </>
   )
