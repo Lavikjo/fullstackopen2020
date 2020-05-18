0f0e77f4785712e552bd9603f49f2b455912c491
Joel Lavikainen <9072078+Lavikjo@users.noreply.github.com>
2020-05-18 14:04:53 +0300

    Fix notification disappearing immediatly

--- osa7/bloglist-frontend/src/reducers/notificationReducer.js
+++ osa7/bloglist-frontend/src/reducers/notificationReducer.js
@@ -13,14 +13,14 @@ export const removeNotification = () => {
   }
 }
 
-const notificationReducer = (state = [], action) => {
+const notificationReducer = (state = null, action) => {
   switch(action.type) {
   case "SET_NOTIFICATION":
     return action.data
   case "REMOVE_NOTIFICATION":
     return null
   default:
-    return null
+    return state
   }
 }
 
