1c494150a7b169c8a48e7651eec6319fc458ec37
Joel Lavikainen <9072078+Lavikjo@users.noreply.github.com>
2020-05-21 16:42:58 +0300

    Implement HTTP-endpoint for BMI calculator

---
+++ osa9/health_calculators/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express"
+import bmiCalculator, { BmiLevel } from "./bmiCalculator"
+const app = express()
+
+app.get("/hello", (_req, res) => {
+  res.send("Hello Full Stack!")
+})
+
+app.get("/bmi", (req, res): void => {
+  const weight: number = Number(req.query.weight)
+  const height: number = Number(req.query.height)
+
+  if (isNaN(weight) || isNaN(height)) {
+    res.status(400).send({ error: "malformatted parameters" })
+    return
+  }
+
+  const bmi: BmiLevel = bmiCalculator(height, weight)
+
+  res.json({
+    weight: weight,
+    height: height,
+    bmi: bmi,
+  })
+})
+
+const PORT = 3003
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
