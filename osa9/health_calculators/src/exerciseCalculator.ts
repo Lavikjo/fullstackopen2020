8a324727ec7dc3e57632418eb1cc9ea750d28e25
Joel Lavikainen <9072078+Lavikjo@users.noreply.github.com>
2020-05-21 16:49:27 +0300

    Separate exercisecalculator to module and cli

--- osa9/health_calculators/src/exerciseCalculator.ts
+++ osa9/health_calculators/src/exerciseCalculator.ts
@@ -1,6 +1,6 @@
 type Description = "bad" | "decent" | "okay" | "good" | "excellent"
 
-interface Result {
+export interface Result {
   periodLength: number
   trainingDays: number
   success: boolean
@@ -52,27 +52,4 @@ const calculateExercise = (target: number, hours: Array<number>): Result => {
   }
 }
 
-interface ExerciseValues {
-  target: number
-  hours: Array<number>
-}
-
-const parseExerciseArguments = (args: Array<string>): ExerciseValues => {
-  if (args.length < 4) throw new Error("Not enough arguments")
-  const hours: Array<number> = args.slice(3).map((x) => +x)
-  if (!isNaN(Number(args[2])) && hours.some((n) => !isNaN(n))) {
-    return {
-      target: Number(args[2]),
-      hours: hours,
-    }
-  } else {
-    throw new Error("Provided values were not numbers!")
-  }
-}
-
-try {
-  const { target, hours } = parseExerciseArguments(process.argv)
-  console.log(calculateExercise(target, hours))
-} catch (e) {
-  console.log("Error, something bad happened, message: ", e.message)
-}
+export default calculateExercise
---
+++ osa9/health_calculators/src/exerciseCli.ts
@@ -0,0 +1,26 @@
+import calculateExercise from "./exerciseCalculator"
+
+interface ExerciseValues {
+  target: number
+  hours: Array<number>
+}
+
+const parseExerciseArguments = (args: Array<string>): ExerciseValues => {
+  if (args.length < 4) throw new Error("Not enough arguments")
+  const hours: Array<number> = args.slice(3).map((x) => +x)
+  if (!isNaN(Number(args[2])) && hours.some((n) => !isNaN(n))) {
+    return {
+      target: Number(args[2]),
+      hours: hours,
+    }
+  } else {
+    throw new Error("Provided values were not numbers!")
+  }
+}
+
+try {
+  const { target, hours } = parseExerciseArguments(process.argv)
+  console.log(calculateExercise(target, hours))
+} catch (e) {
+  console.log("Error, something bad happened, message: ", e.message)
+}
