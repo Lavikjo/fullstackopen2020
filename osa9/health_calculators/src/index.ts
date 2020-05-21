6b491ef0d5859c864d97ea7184d776b37118cffb
Joel Lavikainen <9072078+Lavikjo@users.noreply.github.com>
2020-05-21 18:22:00 +0300

    Complete WebExercises

--- osa9/health_calculators/src/index.ts
+++ osa9/health_calculators/src/index.ts
@@ -1,6 +1,21 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { check, validationResult } from "express-validator";
 import bmiCalculator, { BmiLevel } from "./bmiCalculator";
+import calculateExercise, { Result } from "./exerciseCalculator";
 const app = express();
+app.use(bodyParser.json());
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any 
+app.use(function (error: any, _req: any, res: any, next: () => void) {
+  if (error instanceof SyntaxError) {
+    console.error(error);
+    res.status(400).send({error: "invalid json"});
+    return;
+  } else {
+    next();
+  }
+});
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
@@ -23,6 +38,33 @@ app.get("/bmi", (req, res): void => {
     bmi: bmi,
   });
 });
+
+app.post("/exercises", [
+  check("daily_exercises").not().isEmpty(),
+  check("target").not().isEmpty()
+  
+ ],
+ // eslint-disable-next-line @typescript-eslint/no-explicit-any 
+ (req: any, res: any): void => { 
+  
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+     res.status(400).json({ error: "missing parameters" });
+     return;
+  }
+
+  const hours: Array<number> = req.body.daily_exercises;
+  const target: number = req.body.target;
+  if (isNaN(target) || hours.some(n => isNaN(n))) {
+    res.status(400).send({ error: "malformatted parameters"});
+    return;
+  }
+  const result: Result = calculateExercise(target, hours);
+  
+
+  res.json(result);
+  
+} );
 
 const PORT = 3003;
 
