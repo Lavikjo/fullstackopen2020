53d9912ea3016f7c9c492c31efeb264de43c6dfb
Joel Lavikainen <9072078+Lavikjo@users.noreply.github.com>
2020-09-20 18:00:25 +0300

    Add additional Entry types to data

--- osa9/patientor_backend/src/types/patient.ts
+++ osa9/patientor_backend/src/types/patient.ts
@@ -1,13 +1,53 @@
+import { Diagnosis } from "./diagnose";
+
 export enum Gender { 
   Male = "male",
   Female = "female",
   Other = "other"
 } 
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
-
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
 }
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface Sickleave {
+  startDate: string,
+  endDate: string
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string
+  sickLeave: Sickleave;
+}
+
+interface Discharge {
+  date: string,
+  criteria: string
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+export type Entry = HospitalEntry | OccupationalHealthcareEntry | HealthCheckEntry;
 
 export interface Patient {
     id: string
