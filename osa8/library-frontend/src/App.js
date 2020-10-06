c6a07c2055649f5c62365d0bbbaba64ff3fcb8b1
Joel Lavikainen <9072078+Lavikjo@users.noreply.github.com>
2020-10-06 18:48:24 +0300

    Add author edit page

--- osa8/library-frontend/src/App.js
+++ osa8/library-frontend/src/App.js
@@ -1,31 +1,28 @@
-import React, { useState } from 'react'
-import Authors from './components/Authors'
-import Books from './components/Books'
-import NewBook from './components/NewBook'
+import React, { useState } from "react"
+import AuthorEdit from "./components/AuthorEdit"
+import Authors from "./components/Authors"
+import Books from "./components/Books"
+import NewBook from "./components/NewBook"
 
 const App = () => {
-  const [page, setPage] = useState('authors')
+  const [page, setPage] = useState("authors")
 
   return (
     <div>
       <div>
-        <button onClick={() => setPage('authors')}>authors</button>
-        <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
+        <button onClick={() => setPage("authors")}>authors</button>
+        <button onClick={() => setPage("books")}>books</button>
+        <button onClick={() => setPage("add")}>add book</button>
+        <button onClick={() => setPage("edit")}>edit author</button>
       </div>
 
-      <Authors
-        show={page === 'authors'}
-      />
+      <Authors show={page === "authors"} />
 
-      <Books
-        show={page === 'books'}
-      />
+      <Books show={page === "books"} />
 
-      <NewBook
-        show={page === 'add'}
-      />
+      <NewBook show={page === "add"} />
 
+      <AuthorEdit show={page === "edit"} />
     </div>
   )
 }
---
+++ osa8/library-frontend/src/components/AuthorEdit.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react"
+import { useMutation, useQuery } from "@apollo/client"
+import { ALL_AUTHORS, EDIT_AUTHOR } from "../queries"
+
+const AuthorEdit = (props) => {
+  const [editAuthor] = useMutation(EDIT_AUTHOR)
+  const result = useQuery(ALL_AUTHORS, { pollInterval: 2000 })
+  const [authors, setAuthors] = useState([])
+  const [selection, setSelection] = useState()
+  const [year, setYear] = useState("")
+
+  useEffect(() => {
+    if (result.data) {
+      setAuthors(result.data.allAuthors)
+      setSelection(result.data.allAuthors[0].name)
+    }
+  }, [result])
+
+  const submit = async (event) => {
+    event.preventDefault()
+    const yearInt = Number(year)
+    editAuthor({ variables: { name: selection, setBornTo: yearInt } })
+
+    setYear("")
+  }
+
+  if (!props.show) {
+    return null
+  }
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <h2>Set birthyear</h2>
+        <select
+          id="author"
+          name="author"
+          value={selection}
+          onChange={({ target }) => setSelection(target.value)}>
+          {authors.map((author) => (
+            <option value={author.name} key={author.name}>
+              {author.name}
+            </option>
+          ))}
+        </select>
+        <div>
+          born
+          <input
+            type="number"
+            value={year}
+            onChange={({ target }) => setYear(target.value)}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
+    </div>
+  )
+}
+
+export default AuthorEdit
