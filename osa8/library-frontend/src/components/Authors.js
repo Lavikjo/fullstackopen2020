ddca4dc0b64353d2d8d499660dcb31b5b4ebbdec
Joel Lavikainen <joel.github@lavikjo.com>
2020-10-06 16:03:10 +0300

    Add frontend with author fetching

---
+++ osa8/library-frontend/src/components/Authors.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react"
+import { useQuery } from "@apollo/client"
+import { ALL_AUTHORS } from "../queries"
+
+const Authors = (props) => {
+  const result = useQuery(ALL_AUTHORS)
+  const [authors, setAuthors] = useState([])
+
+  useEffect(() => {
+    if (result.data) {
+      setAuthors(result.data.allAuthors)
+    }
+  }, [result])
+  if (!props.show) {
+    return null
+  }
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Authors
---
+++ osa8/library-frontend/src/queries.js
@@ -0,0 +1,10 @@
+import { gql } from "@apollo/client"
+
+export const ALL_AUTHORS = gql`
+query {
+    allAuthors{
+        name
+        born
+        bookCount
+    }
+}`
